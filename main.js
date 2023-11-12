e90d578ed5d317bc757849d38a626a929fa8f45a
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2023-11-12 10:56:11 +0600

    24 hours problem has solved

--- main.js
+++ main.js
@@ -2,19 +2,26 @@ const clockEl = document.querySelector(".clock");
 const buttons = document.querySelectorAll(".format button");
 setInterval(generateTime, 1000);
 
-// console.log(date)
 function generateTime() {
   const format = clockEl.getAttribute("data-format");
   const date = new Date();
   let hour = date.getHours();
-  let timeStatus;
   const min = date.getMinutes();
   const sec = date.getSeconds();
-  timeStatus = hour > 12 ? "PM" : "AM";
+  let timeStatus = "";
+
   if (format === "12") {
     hour = hour > 12 ? hour % 12 : hour;
+    timeStatus = hour > 11 ? "PM" : "AM";
+  } else {
+    timeStatus = ""; // No AM/PM for 24-hour format
   }
-  clockEl.innerHTML = `<h1>${hour} : ${min} : ${sec} ${timeStatus}</h1>`;
+
+  // Ensure two-digit formatting for minutes and seconds
+  const formattedMin = min < 10 ? "0" + min : min;
+  const formattedSec = sec < 10 ? "0" + sec : sec;
+
+  clockEl.innerHTML = `<h1>${hour} : ${formattedMin} : ${formattedSec} ${timeStatus}</h1>`;
 }
 
 buttons.forEach((button) => {
